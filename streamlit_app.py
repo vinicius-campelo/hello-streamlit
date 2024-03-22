332cc41915778e5fcad661aafbcf69a5e3681957
Vinicius Campelo <74797865+vinicius-campelo@users.noreply.github.com>
2024-03-22 03:10:35 +0000

    add

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,21 @@
 import streamlit as st
+from datetime import datetime, timedelta
 
-st.write('Welcome to Streamlit')
+
+st.query_params["lang"] = ["pt-br"]
+st.query_params.update()
+
+st.title('My First Streamlit App')
+
+number = st.slider("pick number", 0, 200)
+choises = st.multiselect("Pedidos", ["A ENVIAR","ENVIADOS", "FINALIZADOS", "CANCELADOS"])
+name = st.text_input("Nome:")
+st.sidebar.write()
+st.sidebar.button("click")
+
+st.sidebar.date_input(
+      "Selecione um intervalo de datas",
+    format="DD/MM/YYYY"
+)
+
+
